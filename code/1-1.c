3f659b54373a9a2cd3fa673a96d94130e6b7ac0e
H1yeee19 <happylacu@gmail.com>
2026-03-20 09:29:11 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -4,16 +4,16 @@ int main(void) {
   int a = 10, b =5;
 
   int add(int a, int b) {
-    printf("%d + %d = %d",a, b, a+b);
+    printf("%d + %d = %d\n",a, b, a+b);
   }
   int subtract(int a, int b) {
-    printf("%d - %d = %d",a, b, a-b);
+    printf("%d - %d = %d\n",a, b, a-b);
   }
   int multiply(int a, int b) {
-    printf("%d * %d = %d",a, b, a*b);
+    printf("%d * %d = %d\n",a, b, a*b);
   }
   int divide(int a, int b) {
-    printf("%d / %d = %d",a, b, a/b);
+    printf("%d / %d = %d\n",a, b, a/b);
   }
 
 return 0;
